1608ae215f85d74b02dbe8fd6f54af23f8f3322d
MIDASiko <ki907807@gmail.com>
2025-05-01 16:51:39 +0300

    3

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const navLinks = document.querySelectorAll("nav a");
-navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    navLinks.forEach(l => l.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
